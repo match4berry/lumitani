b4b78501a0294009fbec91c42553a79afe8c8182
rismanda <rismandatyas@gmail.com>
2026-03-25 00:45:02 +0700

    update api commoditiy

--- farm-ecommerce/src/server.js
+++ farm-ecommerce/src/server.js
@@ -35,7 +35,7 @@ app.get('/', async (req, res) => {
     const mappingFuncCall = [callProducts, fetchCategories()]
     let [dataProducts, dataCategories] = await Promise.all(mappingFuncCall)
     products = dataProducts.items
-    categories = dataCategories.items || dataCategories || []
+    categories = Array.isArray(dataCategories) ? dataCategories : (dataCategories.items || [])
   }catch(err) {
     console.log(err)
   }
