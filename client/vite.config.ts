ac33176bb894e5f4391e9fc0e1313e2c68b2e938
rismanda <rismandatyas@gmail.com>
2026-03-25 00:28:31 +0700

    update vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    allowedHosts: ['elcyone.my.id', 'www.elcyone.my.id', 'localhost'],
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
